bb01f2ff3fe6eb4ec30fdc2d061112661f07f3fd
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2022-12-14 20:52:41 +0530

    day2 added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Analytics } from "./components/anjali/Analytics";
+import { Channels } from "./components/anjali/Channels";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+    
+    </div>
+  )
 }
 
 export default App;
