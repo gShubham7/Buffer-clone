ee7d38e0a17e455575ff74b3c12969502211e0ce
Sandeep Gond <sandeepgond13sandy@gmail.com>
2022-12-14 20:58:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import "./App.css";
-import { Analytics } from "./components/anjali/Analytics";
-import { Channels } from "./components/anjali/Channels";
-
 function App() {
   return (
     <div className="App">
