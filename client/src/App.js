785d30245ff4cc78aa4e5e8316c250c668da3100
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-12-14 20:27:01 +0530

    day2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Register from "./components/sandeep/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Register/>
+  </div>;
 }
 
 export default App;
---
+++ client/src/redux/store.js
@@ -0,0 +1,22 @@
+import {
+    legacy_createStore,
+    combineReducers,
+    applyMiddleware,
+    compose,
+  } from "redux";
+  import thunk from "redux-thunk";
+  // import { authReducer } from "./auth/auth.reducer";
+  import { registerReducer } from "./register/register.reducer";
+//   import { postReducer } from "./posts/post.reducer";
+  
+  const rootReducer = combineReducers({
+      register: registerReducer,
+      // auth: authReducer,
+    // posts: postReducer,
+  });
+  
+  const createComposer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+  export const store = legacy_createStore(
+    rootReducer,
+    createComposer(applyMiddleware(thunk))
+  );
