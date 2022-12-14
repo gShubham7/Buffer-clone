19dda9c345eb6c9d8e7b094c60c8c1814f270fec
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-12-14 20:42:10 +0530

    day2

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+// import Login from "./components/sandeep/Login";
 import Register from "./components/sandeep/Register";
 
 function App() {
   return <div className="App">
+    {/* <Login/> */}
     <Register/>
   </div>;
 }
