ad21a0bcaed58c25d90cef95d216b0a0455d07b0
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-12-15 19:48:54 +0530

    Day3

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./components/sandeep/Login";
+import Register from "./components/sandeep/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Login/>
+    {/* <Register/> */}
+  </div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ChakraProvider>
-    <Provider store={store}>
-
-    <App />
+      <ChakraProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 reportWebVitals();
