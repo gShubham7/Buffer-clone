223e1642be2feb9604ff1c38a8c591872fea05c5
Shubham Gaikwad <107462943+gShubham7@users.noreply.github.com>
2022-12-15 09:58:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
-import { NavLink } from "react-router-dom";
+
 import "./App.css";
-import FBPost from "./components/shubham/fbPosts";
-import { Publishing } from "./pages/Publishing";
-import AllRoutes from "./routes/AllRoutes";
+
 
 function App() {
-  return (
-    <div className="App">
-      {/* <AllRoutes /> */}
-      {/* <NavLink to="/publishing">Publish</NavLink> */}
-      <Publishing />
-    </div>
-  );
+ 
 }
 
 export default App;
