5d33104da0871b860a684fea27ea994a36144f8a
Shubham Gaikwad <107462943+gShubham7@users.noreply.github.com>
2022-12-15 10:00:22 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
-
+// import Login from "./components/sandeep/Login";
+import Register from "./components/sandeep/Register";
 
 function App() {
- 
+  return <div className="App">
+    {/* <Login/> */}
+    <Register/>
+  </div>;
 }
 
 export default App;
