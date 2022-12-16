b9acaa5c00cb822da7686eba0bf7eecb0ea1595f
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2022-12-16 20:31:04 +0530

    day4

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Publishing } from "./pages/Publishing";
+
 function App() {
-  return (
-    <div className="App"></div>
-  )
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
